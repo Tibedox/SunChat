cf1b8222cd832f9bcdb52cce8ebe0535bb3a4025
Tibedox <tibedox@mail.ru>
2025-04-06 16:05:44 +0300

    sendMessage

--- app/src/main/java/com/example/sunchat/ChatActivity.java
+++ app/src/main/java/com/example/sunchat/ChatActivity.java
@@ -1,6 +1,11 @@
 package com.example.sunchat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ChatActivity extends AppCompatActivity {
+    String name;
+    ListView listView;
+    EditText editText;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,16 @@ public class ChatActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Intent intent = getIntent();
+        name = intent.getStringExtra("name");
+        listView = findViewById(R.id.listView);
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+    }
+
+    public void sendMessage(View view) {
+        if(editText.getText().toString().isEmpty()) return;
+
     }
 }
